5a96ac2b9bbb0139cd32b7f59c7464f61f9abf0e
Nicholas Pfleegor <ndp5030@gmail.com>
2022-05-31 01:37:39 -0400

    Assignment almost finished with minor issues

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -11,4 +11,10 @@ FROM techjobs.employers
 WHERE Location = "St. Louis City";
 
 --Part 3
+SELECT JobId, techjobs.skills.Name, techjobs.skills.Description 
+FROM techjobs.jobskills
+INNER JOIN techjobs.skills ON techjobs.jobskills.SkillId = techjobs.skills.Id
+ORDER BY Name;
 
+-- Why do we need to use is not null when we can inner join?
+-- I read the instructions. Not sure if it wants alphabetical by job name or skill name
